05efb0770428621c777a3171d2a9e6024c2a700b
Raving-hash <Raving-hash@foxmail.com>
2021-12-12 14:32:36 +0800

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] exchange(int[] nums) {
+        int start= 0;
+        int end = nums.length;
+        int[] res = new int[end];
+        for (int n: nums){
+            if(n % 2 == 1){
+                res[start] = n;
+                start++;
+            }else{
+                res[end-1] = n;
+                end--;
+            }
+        }
+        return  res;
+    }
+}
